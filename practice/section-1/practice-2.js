4c137ae448e3ac0b43db7f6a409ec0c8e3d4ed60
BILLY MAI (ITA-ISDC-ISD-OOCLL/ZHA) <MAIBI@OOCL.COM>
2019-07-08 11:57:54 +0800

    finished

--- practice/section-1/practice-2.js
+++ practice/section-1/practice-2.js
@@ -1,5 +1,16 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+  var arr = new Array();
+ // console.log(collectionB[0]);
+ var collectionB1 = collectionB[0]
+  for(var i in collectionA){
+    for(var j in collectionB1){
+        if(collectionA[i]==collectionB1[j]){
+          arr.push(collectionA[i]);
+          //console.log(arr);
+        }
+    }
+  }
+  return arr;
 }
--- practice/section-1/practice-3.js
+++ practice/section-1/practice-3.js
@@ -1,5 +1,16 @@
 'use strict';
 
 function collectSameElements(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  var arr = new Array();
+  // console.log(collectionB[0]);
+  var collectionB = objectB.value;
+   for(var i in collectionA){
+     for(var j in collectionB){
+         if(collectionA[i]==collectionB[j]){
+           arr.push(collectionA[i]);
+           //console.log(arr);
+         }
+     }
+   }
+   return arr;
 }
